3e842172e8c92c94916f933cc04bc79e301efc5e
Sachin-Malaghan <sachinmalaghan1973@gmail.com>
2026-04-22 23:18:24 +0530

    Update ConsolePanel.cpp

--- Editor/src/UI/Panels/ConsolePanel.cpp
+++ Editor/src/UI/Panels/ConsolePanel.cpp
@@ -205,6 +205,9 @@ namespace { namespace ConsoleFn {
 }}
 
 void ConsolePanel::OnUIRender() {
+    static ImGuiContext* s_Ctx = ImGui::GetCurrentContext();
+    if (!s_Ctx) return;
+    ImGui::SetCurrentContext(s_Ctx);
     ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
     bool windowOpen = ImGui::Begin("Console", nullptr, ImGuiWindowFlags_MenuBar);
     ImGui::PopStyleVar();
